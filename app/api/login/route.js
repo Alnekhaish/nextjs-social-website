f7d0511300ed4a80ea744c34bee1b8ec1ae4d671
Abdullah <alnekhaish.a@gmail.com>
2023-10-19 07:44:50 +0300

    disable cors at api/login

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -23,12 +23,8 @@ export async function POST(request) {
         {
           status: 200,
           headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Set-Cookie": `token=${
-              response.data.token
-            }; Expires=${date.toUTCString()}; Path=/`,
+            "Set-Cookie": `token=${response.data.token
+              }; Expires=${date.toUTCString()}; Path=/`,
           },
         },
       );
