bd39e1f7b0cc139fe8b5e07b085ca173296d04be
Abdullah <alnekhaish.a@gmail.com>
2023-10-19 07:35:55 +0300

    allow cors at api/login

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -23,6 +23,9 @@ export async function POST(request) {
         {
           status: 200,
           headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
             "Set-Cookie": `token=${
               response.data.token
             }; Expires=${date.toUTCString()}; Path=/`,
